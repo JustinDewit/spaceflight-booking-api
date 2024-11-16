dedb79cb7b8b0c1b48ca41d7af25d32fd64ae177
JustinDewit <137382666+JustinDewit@users.noreply.github.com>
2024-11-16 13:18:44 +0100

    feat: setup Route model

--- backend/models/Route.ts
+++ backend/models/Route.ts
@@ -1,7 +1,65 @@
 import mongoose from 'mongoose';
 
 const routeSchema = new mongoose.Schema({
-    // schema definition
+  flightNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  departure: {
+    location: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    }
+  },
+  arrival: {
+    location: {
+      type: String,
+      required: true,
+    },
+    estimatedDate: {
+      type: Date,
+      required: true,
+    }
+  },
+  distance: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  duration: {
+    type: Number, // Duration in hours
+    required: true,
+    min: 0,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  availableSeats: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  status: {
+    type: String,
+    enum: ['SCHEDULED', 'IN_FLIGHT', 'COMPLETED', 'CANCELLED'],
+    default: 'SCHEDULED',
+  },
+  spacecraft: {
+    type: String,
+    required: true,
+    default: 'Starship',
+  },
+  description: {
+    type: String,
+    maxLength: 1000,
+  }
 });
 
 const Route = mongoose.model('Route', routeSchema);
